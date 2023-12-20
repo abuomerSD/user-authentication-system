5a3d94bdd519aebaf13711701be0f3318cecedbc
asdf <tayeb1293@yahoo.com>
2023-12-20 07:45:50 +0200

    feature: update and delete user now works , and  working on login function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -44,6 +44,9 @@ async function testDb(){
 const getAllUsers = async (req, res) => {
     try {
         const users = await User.findAll();
+        if(users.length < 1) {
+            return res.json({message: 'No users'});
+        }
         res.json(users);
     } catch (error) {
         res.json(error);
@@ -70,33 +73,84 @@ const createUser = async (req, res) => {
 const updateUser = async (req, res) => {
     
     try {
-        // let user = await User.findOne({
+        // let user = {}
+        // console.log(`id: ${req.params.id}`)
+        // user.username = req.body.username;
+        // const salt = await bcrypt.genSalt();
+        // const password = req.body.password;
+        // const hashedPassword = await bcrypt.hash(password, salt);
+        // user.password = hashedPassword;
+        // const updateUser = await User.update({user}, {
         //     where: {
         //         id: req.params.id,
         //     }
         // })
-        // console.log(user);
-        let user = {}
-        user.username = req.body.username;
-        const salt = await bcrypt.genSalt();
-        const password = req.body.password;
-        const hashedPassword = await bcrypt.hash(password, salt);
-        user.password = hashedPassword;
-        await User.update({user}, {
+        //     .then(res.json(updateUser));
+
+        const {username, password} = req.body;
+        const user = await User.findOne({
             where: {
                 id: req.params.id,
             }
         });
+        user.username = username;
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password , salt);
+        user.password = hashedPassword;
+
+        user.save({fields:['username', 'password']});
         res.json(user);
+
+        
 
     } catch (error) {
         res.json(error);
     }
 }
 
+const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                id: req.params.id,
+            }
+        });
+        await User.destroy({
+            where: {
+                id: req.params.id,
+            }
+        });
+        res.json({message: `${user.username} has been deleted`});
+    } catch (error) {
+        res.json(error);
+    }
+}
+
+exports.login = async (req, res) => {
+    const { username, password } = req.body;
+    if(!username || !password) {
+        return res.status(400).json({
+            message: 'username or password not provided',
+        });
+    }
+
+    try {
+        const user = await User.findOne({
+            where: {
+                id: req.params.id,
+            }
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: "An error occurred",
+            error: error.message,
+          })
+    }
+}
 
 module.exports = {
     getAllUsers,
     createUser,
     updateUser,
+    deleteUser,
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, createUser, updateUser } = require('../controllers/usersController');
+const { getAllUsers, createUser, updateUser, deleteUser , login} = require('../controllers/usersController');
 
-router.get('/', getAllUsers);
-router.post('/', createUser);
-router.put('/:id', updateUser);
+// router.get('/', getAllUsers);
+// router.post('/', createUser);
+// router.put('/:id', updateUser);
+// router.delete('/:id', deleteUser);
+
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:id').put(updateUser).delete(deleteUser);
+router.route('/login').post(login);
 
 
 module.exports = router;
