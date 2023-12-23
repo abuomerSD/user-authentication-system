8e2033a024eabef0842bbd188be682962af46288
asdf <tayeb1293@yahoo.com>
2023-12-23 07:31:55 +0200

    feature: adding jwt

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,7 @@
 const { Sequelize, DataTypes, Model, where } = require('sequelize');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
 const dbURI = "postgres://asdf:''@localhost:5432/auth";
 const sequelize = new Sequelize(dbURI);
@@ -12,6 +14,7 @@ User.init({
     username:{
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
     },
     password: {
         type: DataTypes.STRING,
@@ -126,7 +129,7 @@ const deleteUser = async (req, res) => {
     }
 }
 
-exports.login = async (req, res) => {
+const login = async (req, res) => {
     const { username, password } = req.body;
     if(!username || !password) {
         return res.status(400).json({
@@ -137,9 +140,29 @@ exports.login = async (req, res) => {
     try {
         const user = await User.findOne({
             where: {
-                id: req.params.id,
+                username
             }
-        })
+        });
+
+        if(!user) {
+            return res.status(400).json({
+                message: 'user not found',
+            });
+        }
+
+        bcrypt.compare(password, user.password)
+            .then( (result) => {
+                if(result){
+                    res.json({
+                        message: 'login successfully',
+                    })
+                }
+                else {
+                    res.json({
+                        message: 'password not match',
+                    })
+                }
+            } )
     } catch (error) {
         res.status(400).json({
             message: "An error occurred",
@@ -148,9 +171,51 @@ exports.login = async (req, res) => {
     }
 }
 
+const register = async (req, res) => {
+    const jwtSecret = process.env.JWT_SECRET ;
+    const {username, password} = req.body;
+
+    if(!username || !password) {
+        return res.status(400).json({
+            message: 'please enter the username and the password'
+        })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({
+        username,
+        password: hashedPassword,
+    })
+        .then((user)=> {
+            const maxAge = 3 * 60 * 60;
+            const token = jwt.sign(
+                {id: user.id, username, role:user.role},
+                jwtSecret,
+                {
+                    expiresIn: maxAge, // 3 hrs in sec
+                }
+            )
+            res.cookie('jwt', token,{
+                httpOnly: true,
+                maxAge: maxAge * 1000, // 3 hrs in ms
+            })
+            res.status(201).json({
+                message: 'user added successfully',
+            })
+        })
+        .catch((error) =>
+        res.status(400).json({
+          message: "User not successful created",
+          error: error.message,
+        })
+
+}
+
 module.exports = {
     getAllUsers,
     createUser,
     updateUser,
     deleteUser,
+    login,
+    register,
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, createUser, updateUser, deleteUser , login} = require('../controllers/usersController');
+const { getAllUsers,
+        createUser,
+        updateUser,
+        deleteUser,
+        login } = require('../controllers/usersController');
 
-// router.get('/', getAllUsers);
-// router.post('/', createUser);
-// router.put('/:id', updateUser);
-// router.delete('/:id', deleteUser);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').put(updateUser).delete(deleteUser);
